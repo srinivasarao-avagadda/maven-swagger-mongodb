24411a41f68af81617f35498569e8c3717df4570
srini <srini@LAPTOP-1F30DL74>
2020-04-30 00:28:52 +0530

    added dummy packages

--- src/main/java/com/mphasis/controller/CarController.java
+++ src/main/java/com/mphasis/controller/CarController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mphasis.entity.Car;
@@ -24,47 +25,53 @@ import io.swagger.annotations.ApiResponses;
 @RequestMapping(value = "/")
 public class CarController {
 
-	private final Logger LOG = LoggerFactory.getLogger(getClass());
-	
-	@Autowired
-	CarService carService;
+  private final Logger LOG = LoggerFactory.getLogger(getClass());
 
-	@ApiOperation(value = "creates a car", notes = "based on reponsebody as input it creates car document")
-	@PostMapping(value = "/create")
-	public Car createCar(@RequestBody Car car) {
-		LOG.info("Saving Car.");
+  @Autowired
+  CarService carService;
+
+  @GetMapping(value = "/ping")
+  public @ResponseBody String respondToPing() {
+    long ts = System.currentTimeMillis();
+    LOG.info("Login Controller pinged. ts:" + ts);
+    return "Login -- responding to ping at " + ts;
+  }
+
+  @ApiOperation(value = "creates a car", notes = "based on reponsebody as input it creates car document")
+  @PostMapping(value = "/create")
+  public Car createCar(@RequestBody Car car) {
+    LOG.info("Saving Car.");
     return carService.create(car);
-	}
-	
-	@ApiOperation(value = "get a car", notes = "get a car")
-	@ApiResponses(value = { @ApiResponse(code = 400, message = "Invalid car supplied"),
-    @ApiResponse(code = 404, message = "Car not found") })
+  }
+
+  @ApiOperation(value = "get a car", notes = "get a car")
+  @ApiResponses(value = {@ApiResponse(code = 400, message = "Invalid car supplied"),
+    @ApiResponse(code = 404, message = "Car not found")})
   @GetMapping(value = "/get")
   public Car getCar(@RequestBody Car car) {
     LOG.info("Get Car.");
     return carService.find(car).get();
   }
-	
-	@ApiOperation(value = "update a car", notes = "update a car")
+
+  @ApiOperation(value = "update a car", notes = "update a car")
   @PutMapping(value = "/update")
-  public void updateCar(@ApiParam(value = "Updated car object", required = true)  @RequestBody Car car) {
+  public void updateCar(@ApiParam(value = "Updated car object", required = true) @RequestBody Car car) {
     LOG.info("Put a Car.");
     carService.update(car);
   }
-	
-	@ApiOperation(value = "delete a car", notes = "update a car")
+
+  @ApiOperation(value = "delete a car", notes = "update a car")
   @PutMapping(value = "/delete")
   public void deleteCar(@RequestBody Car car) {
     LOG.info("Put a Car.");
     carService.update(car);
   }
-	
-	@ApiOperation(value = "delete a car", notes = "update a car")
+
+  @ApiOperation(value = "delete a car", notes = "update a car")
   @PutMapping(value = "/deleteAll")
   public void deleteAllCars(@RequestBody Car car) {
     LOG.info("Delete all cars.");
     carService.deleteAll();
   }
-	
-	
+
 }
---
+++ src/main/java/com/mphasis/util/package-info.java
@@ -0,0 +1,1 @@
+package com.mphasis.util;
