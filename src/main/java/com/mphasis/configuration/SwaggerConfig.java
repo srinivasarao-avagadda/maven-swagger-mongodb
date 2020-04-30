eba92b3dd1cf728f77a119841426202f3af59eb4
srini <srini@LAPTOP-1F30DL74>
2020-04-30 12:12:56 +0530

    refactor changes

--- src/main/java/com/mphasis/configuration/SwaggerConfig.java
+++ src/main/java/com/mphasis/configuration/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package com.mphasis.configuration;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
